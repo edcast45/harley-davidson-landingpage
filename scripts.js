602570c1fc3cd6ff91afe857ff3733e0f8796afe
Edwin Castro <59403733+Edwin-Castro@users.noreply.github.com>
2021-08-01 20:17:13 -0400

    Used different css code for slide-in menu

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.main-description');
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+  lineOne.classList.toggle('line-cross');
+  lineTwo.classList.toggle('line-fade-out');
+  lineThree.classList.toggle('line-cross');
+  link.classList.toggle('fade-in');
 })
